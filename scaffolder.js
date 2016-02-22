0968d85fa5ff92a4813b8459fce4141df39f2331
azthief <choiyg81@gmail.com>
2016-02-22 23:11:14 +0900

    First Runnable Version

--- scaffolder.js
+++ scaffolder.js
@@ -8,17 +8,42 @@ Mean Stack Scaffold Maker Test
 
 var fs = require('fs');
 
-//Checking File Exist
-var fileNm = 'std2.js';
-fs.stat(fileNm, function(err, stats) {
-	if(stats === undefined) {
-		createFile(fileNm);
-	}
+ask('Name', /.+/, function(name) {
+    createFile(name);
 });
 
 function createFile(fileNm) {
-	fs.writeFile(fileNm, "'strict use'", function(err) {
-		if(err) throw err;
-		console.log('it\'s saved!');
-	});
+    //Checking File Exist
+    fs.stat(fileNm, function(err, stats) {
+        if (stats !== undefined) {
+            console.log(fileNm + ' is already exist!');
+        }
+        process.exit();
+    });
+
+    //Create File
+    fs.writeFile(fileNm, "'strict use'", function(err) {
+        if (err) throw err;
+        console.log('it\'s saved!');
+        process.exit();
+    });
+}
+
+function ask(question, format, callback) {
+    var stdin = process.stdin,
+        stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(question + ": ");
+
+    stdin.once('data', function(data) {
+        data = data.toString().trim();
+
+        if (format.test(data)) {
+            callback(data);
+        } else {
+            stdout.write('It should match: ' + format + '\n');
+            ask(question, format, callback);
+        }
+    });
 }
